e73e8899a5f8af5d5ace995a26a59569c4a81a96
mattia fiorio <mattia.fiorio@akqa.com>
2026-08-10 14:26:13 +0200

    adding mcp, had to downgrade mcp library

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from models.Product import ProductSchema, Product
 from config import get_db
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_mcp import FastApiMCP
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],  # usual frontend url (react)
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+mcp = FastApiMCP(app)
+# Mount the MCP server directly to your FastAPI app
+mcp.mount()
 
 @app.get("/")
 def greet():
