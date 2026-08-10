4db79880c005dc7c78f859fc18e6874ba1d2c32d
mattia fiorio <mattia.fiorio@akqa.com>
2026-08-10 12:01:45 +0200

    basic crud

---
+++ docs.md
@@ -0,0 +1,6 @@
+# Notes
+
+Running pip will install packages system wise. A better idea is creating an environment: `python -m venv ENV_NAME`
+From inside the environment we can then install packages `pip install fastapi uvicorn`
+
+Fast API comes with Swagger at /docs: `http://localhost:8000/docs`
--- main.py
+++ main.py
@@ -1,7 +1,54 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from models import Product
 
 app = FastAPI()
 
 @app.get("/")
 def greet():
     return "Hello, World!"
+
+products = [Product(id=1, name="Product 1", description="Description 1", price=100, quantity=100)]
+
+@app.get("/products")
+def get_products():
+    return products
+
+@app.get("/product/{id}")
+def get_products_by_id(id: int):
+    # in js terms[product.id for product in products] is the same as products.map(product => product.id)
+    if id not in [product.id for product in products]:
+        raise HTTPException(status_code=404, detail="Product not found")
+   
+    # for product in products - iterates over products
+    # if product.id == id - only considers items with this condition
+    # product (at the start) - what to yield out of this function 
+    # next() - returns the first item that satisfies the condition. It's lazy and doesn't compute all items.
+    product = next(product for product in products if product.id == id)
+    return product
+
+@app.post("/product")
+def create_product(product: Product):
+    products.append(product)
+    return product
+
+@app.put("/product/{id}")
+def update_product(id: int, product: Product):
+
+    # The old way of doing this is:
+    #   for i in range(len(products)):
+    #     p = products[i]
+    #   enumerate(products) - returns a tuple of (index, product), so it's preferred
+    for i, p in enumerate(products):
+        if p.id == id:
+            products[i] = product
+            return product
+    raise HTTPException(status_code=404, detail="Product not found")
+
+@app.delete("/product/{id}")
+def delete_product(id: int):
+    productToDelete = next((product for product in products if product.id == id), None)
+    if productToDelete is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+
+    products.remove(productToDelete)
+    return {"message": "Product deleted successfully"}
